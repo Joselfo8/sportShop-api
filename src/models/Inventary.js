aec07a4319d9741053550eb95d889a8451061afc
Hans Garcia <hansvekoni@gmail.com>
2022-07-05 20:45:44 -0300

    Validaciones de Inventary

--- src/models/Inventary.js
+++ src/models/Inventary.js
@@ -6,11 +6,22 @@ module.exports = (sequelize) => {
     {
       talla: {
         type: DataTypes.STRING,
+        validate:{
+          notEmpty : {msg : 'The Talla is required!'},
+          isAlphanumeric: true, msg : 'The Talla must be a number',
+          len: [1,5], msg : 'The Talla must be between 1 and 5 characters', 
+        }
+        
       },
       cantidad: {
         type: DataTypes.INTEGER,
         allowNull: false,
         defaultValue: 0,
+        Validate : {
+          notEmpty : {msg : 'The Cantidad is required!'},
+          isNumeric: true, msg : 'The Cantidad must be a number',
+          len:[1,10],msg:'The Cantidad must be between 1 and 10 characters',
+        }
       },
     },
     {
